497f268df3bfa18a6f1ef8d1c90d7e857a316d8b
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-02 16:11:57 -0500

    Added constants

---
+++ controllers/constants.js
@@ -0,0 +1,21 @@
+const CREATE_SUCCESS = "Successfully created.";
+const UPDATE_SUCCESS = "Successfully updated.";
+const GET_SUCCESS = "Successfully retrieved.";
+const DELETE_SUCCESS = "Successfully deleted.";
+const CREATE_FAIL = "Could not be created.";
+const UPDATE_FAIL = "Could not be updated.";
+const GET_FAIL = "Could not be retrieved.";
+const DELETE_FAIL = "Could not be deleted.";
+const NOT_FOUND = "Not found."
+
+module.exports = {
+    CREATE_FAIL,
+    CREATE_SUCCESS,
+    UPDATE_FAIL,
+    UPDATE_SUCCESS,
+    DELETE_FAIL,
+    DELETE_SUCCESS,
+    GET_FAIL,
+    GET_SUCCESS,
+    NOT_FOUND
+}
