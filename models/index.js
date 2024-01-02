ddf89025271173a816c38993396d1ba1cec955d0
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-02 14:41:11 -0500

    Imported models

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const UserModel = require('./user');
+const {BookModel, ChapterModel, TagModel} = require('./book');
+
+
+
+module.exports ={
+    UserModel,
+    BookModel,
+    ChapterModel,
+    TagModel,
+};
