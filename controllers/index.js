ee596b2f8c71d8e2514ae91cfb39eed375b94400
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-02 15:45:42 -0500

    added controller imports

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const UserController = require('./userController');
+const {BookController, ChapterController, TagController} = require('./bookController');
+
+
+
+module.exports ={
+    UserController,
+    BookController,
+    ChapterController,
+    TagController,
+};
