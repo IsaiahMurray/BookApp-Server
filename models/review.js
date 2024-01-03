cdb4fb41ce2d6109af45b93f4c9809386c5459ea
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-03 08:17:31 -0500

    Cleaning up

--- models/review.js
+++ models/review.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require('sequelize');
 const db = require('../db');
-const {UserModel, BookModel} = require('.'); 
 
 const Review = db.define('review', {
     rating: {
@@ -17,8 +16,4 @@ const Review = db.define('review', {
     },
   });
   
-  // Define associations between models
-  Review.belongsTo(User); // A review belongs to a user
-  Review.belongsTo(Book); // A review is associated with a book
-
   module.exports = Review;
