a26495e1c25d7ee7531e2d53ede156a5a5716407
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-04 15:27:01 -0500

    Added notes to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,109 +4,91 @@ const Services = require("../services/index");
 const UserController = require("express").Router();
 const { ValidateSession } = require("../middleware");
 
+// Constants for error messages or success messages are imported from a separate file
 const {
-    INCORRECT_EMAIL_PASSWORD,
-    CREATE_SUCCESS,
-    UPDATE_SUCCESS,
-    UPDATE_FAIL,
-    TITLE_LOGIN_ERROR,
-    TITLE_SIGNUP_ERROR,
-    DELETE_FAIL,
-    DELETE_SUCCESS,
-    USER_FOUND
-  } = require("../controllers/constants");
+  INCORRECT_EMAIL_PASSWORD,
+  CREATE_SUCCESS,
+  UPDATE_SUCCESS,
+  UPDATE_FAIL,
+  TITLE_LOGIN_ERROR,
+  TITLE_SIGNUP_ERROR,
+  DELETE_FAIL,
+  DELETE_SUCCESS,
+  USER_FOUND,
+} = require("../controllers/constants");
 
-  //* User register
-  UserController.route("/register").post(async (req, res) => {
-    try {
-      const { username, email, password } = req.body;
-  
-      const hashedPassword = await Services.PasswordService.hashPassword(password);
-      const userId = await Services.UserService.create({
-        username,
-        email,
-        password: hashedPassword,
-      });
-  
-      res.json({
-        userId,
-        info: {
-          message: CREATE_SUCCESS,
-        },
-      });
-    } catch (e) {
-      if (e instanceof Error) {
-        const errorMessage = {
-          title: TITLE_SIGNUP_ERROR,
-          info: {
-            message:
-              e.message === "Validation error" ? e.original.detail : e.message,
-          },
-        };
-        res.send(errorMessage);
-      }
-    }
-  });
-
-  //* User login
-  UserController.route("/login").post(async (req, res) => {
-    try {
-      const { email, password } = req.body;
-  
-      if (!email || !password) throw new Error(INCORRECT_EMAIL_PASSWORD);
-  
-      const foundUser = await Services.UserService.getByEmail(email);
-  
-      if (!foundUser) throw new Error(INCORRECT_EMAIL_PASSWORD);
-      if (
-        !(await Services.PasswordService.validatePassword(password, foundUser.password))
-      )
-        throw new Error(INCORRECT_EMAIL_PASSWORD);
-  
-      const userId = foundUser?.id;
-      const token = await Services.JWTService.createSessionToken(userId);
-  
-      res.json({
-        user: foundUser,
-        token: token,
-        info: {
-          message: USER_FOUND,
-        },
-      });
-    } catch (e) {
-      if (e instanceof Error) {
-        const errorMessage = {
-          title: TITLE_LOGIN_ERROR,
-          info: {
-            message: e.message,
-          },
-        };
-        res.send(errorMessage);
-      }
-    }
-  });
-
-//* Update username
-UserController.route("/update/username").put(ValidateSession, async (req, res) => {
+//* User register
+UserController.route("/register").post(async (req, res) => {
+  // Register a new user with provided username, email, and password
+  // Hash password before storing in the database
+  // Return success message and user ID upon successful registration
   try {
-    const { username } = req.body;
-    const { id } = req.user;
+    const { username, email, password } = req.body;
 
-    const updatedUserName = await Services.UserService.modifyUsername(
-      id,
-      username
+    const hashedPassword = await Services.PasswordService.hashPassword(
+      password
     );
+    const userId = await Services.UserService.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
     res.json({
-      user: updatedUserName,
+      userId,
       info: {
-        message: UPDATE_SUCCESS,
-        username: username
+        message: CREATE_SUCCESS,
       },
     });
   } catch (e) {
     if (e instanceof Error) {
       const errorMessage = {
-        title: UPDATE_FAIL,
+        title: TITLE_SIGNUP_ERROR,
+        info: {
+          message:
+            e.message === "Validation error" ? e.original.detail : e.message,
+        },
+      };
+      res.send(errorMessage);
+    }
+  }
+});
+
+//* User login
+UserController.route("/login").post(async (req, res) => {
+  // Authenticate user with provided email and password
+  // Create a session token upon successful login
+  // Return user details, token, and success message upon successful login
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) throw new Error(INCORRECT_EMAIL_PASSWORD);
+
+    const foundUser = await Services.UserService.getByEmail(email);
+
+    if (!foundUser) throw new Error(INCORRECT_EMAIL_PASSWORD);
+    if (
+      !(await Services.PasswordService.validatePassword(
+        password,
+        foundUser.password
+      ))
+    )
+      throw new Error(INCORRECT_EMAIL_PASSWORD);
+
+    const userId = foundUser?.id;
+    const token = await Services.JWTService.createSessionToken(userId);
+
+    res.json({
+      user: foundUser,
+      token: token,
+      info: {
+        message: USER_FOUND,
+      },
+    });
+  } catch (e) {
+    if (e instanceof Error) {
+      const errorMessage = {
+        title: TITLE_LOGIN_ERROR,
         info: {
           message: e.message,
         },
@@ -116,18 +98,54 @@ UserController.route("/update/username").put(ValidateSession, async (req, res) =
   }
 });
 
+//* Update username
+UserController.route("/update/username").put(
+  ValidateSession,
+  async (req, res) => {
+    try {
+      // Update user's username based on provided input
+      // Return updated username and success message upon successful update
+      const { username } = req.body;
+      const { id } = req.user;
+
+      const updatedUserName = await Services.UserService.modifyUsername(
+        id,
+        username
+      );
+      res.json({
+        user: updatedUserName,
+        info: {
+          message: UPDATE_SUCCESS,
+          username: username,
+        },
+      });
+    } catch (e) {
+      if (e instanceof Error) {
+        const errorMessage = {
+          title: UPDATE_FAIL,
+          info: {
+            message: e.message,
+          },
+        };
+        res.send(errorMessage);
+      }
+    }
+  }
+);
+
 //* Update email
 UserController.route("/update/email").put(ValidateSession, async (req, res) => {
   try {
+    // Update user's email based on provided input
+    // Return updated email and success message upon successful update
     const { email } = req.body;
     const { id } = req.user;
-
     const updatedUserEmail = await Services.UserService.modifyEmail(id, email);
     res.json({
       user: updatedUserEmail,
       info: {
         message: UPDATE_SUCCESS,
-        email: email
+        email: email,
       },
     });
   } catch (e) {
@@ -144,13 +162,18 @@ UserController.route("/update/email").put(ValidateSession, async (req, res) => {
 });
 
 //* Update password
-UserController
-  .route("/update/password")
-  .put(ValidateSession, async (req, res) => {
+UserController.route("/update/password").put(
+  ValidateSession,
+  async (req, res) => {
+    // Update user's password based on provided input
+    // Hash the new password before storing in the database
+    // Return success message upon successful update
     try {
       const { password } = req.body;
       const { id } = req.user;
-      const hashedPassword = await Services.PasswordService.hashPassword(password);
+      const hashedPassword = await Services.PasswordService.hashPassword(
+        password
+      );
       const updatedUserPass = await Services.UserService.modifyPassword(
         id,
         hashedPassword
@@ -159,7 +182,7 @@ UserController
         user: updatedUserPass,
         info: {
           message: UPDATE_SUCCESS,
-          password: password
+          password: password,
         },
       });
     } catch (e) {
@@ -173,12 +196,15 @@ UserController
         res.send(errorMessage);
       }
     }
-  });
+  }
+);
 
 //! User Archive Account
 
 //* User delete
 UserController.route("/delete").delete(ValidateSession, async (req, res) => {
+  // Delete user account based on user ID from the session
+  // Return success message upon successful account deletion
   try {
     const { id } = req.user;
     const destroyedUser = await Services.UserService.remove(id);
@@ -201,5 +227,5 @@ UserController.route("/delete").delete(ValidateSession, async (req, res) => {
     }
   }
 });
-  
-  module.exports = UserController;
+
+module.exports = UserController;
