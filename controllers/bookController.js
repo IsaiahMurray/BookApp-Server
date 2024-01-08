f307e0f9f5c6ae476ec28927a77adebc76cb7438
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-08 10:45:58 -0500

    Building book controller routes

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,132 @@
 const Services = require("../services/index");
 const BookController = require("express").Router();
+const { ValidateSession } = require("../middleware");
 
+// Constants for error messages or success messages are imported from a separate file
+const {
+  CREATE_SUCCESS,
+  CREATE_FAIL,
+  GET_SUCCESS,
+  GET_FAIL,
+  UPDATE_SUCCESS,
+  UPDATE_FAIL,
+  DELETE_FAIL,
+  DELETE_SUCCESS,
+  NOT_FOUND,
+} = require("../controllers/constants");
+
+//! Create Book
+BookController.route("/create").post(async (req, res) => {
+  try {
+    const {author, title, description, titleFont, contentFont, privacy, canRate, rating, tags, } = req.body;
+  } catch (e) {
+    if (e instanceof Error) {
+      const errorMessage = {
+        title: CREATE_FAIL,
+        info: {
+          message: e.message,
+        },
+      };
+      res.send(errorMessage);
+    }
+  }
+});
+//! Get All Books
+BookController.route("/get/all").get(async (req, res) => {
+    try {
+    
+    } catch (e) {
+      if (e instanceof Error) {
+        const errorMessage = {
+          title: GET_FAIL,
+          info: {
+            message: e.message,
+          },
+        };
+        res.send(errorMessage);
+      }
+    }
+});
+//! Get All Books by User
+BookController.route("get/:userId").get(async (req, res) => {
+    try {
+    
+    } catch (e) {
+      if (e instanceof Error) {
+        const errorMessage = {
+          title: GET_FAIL,
+          info: {
+            message: e.message,
+          },
+        };
+        res.send(errorMessage);
+      }
+    }
+});
+//! Get single Book
+BookController.route("get/:id").get(async (req, res) => {
+    try {
+    
+    } catch (e) {
+      if (e instanceof Error) {
+        const errorMessage = {
+          title: GET_FAIL,
+          info: {
+            message: e.message,
+          },
+        };
+        res.send(errorMessage);
+      }
+    }
+});
+//! Update Book
+BookController.route("update/:id").put(async (req, res) => {
+    try {
+    
+    } catch (e) {
+      if (e instanceof Error) {
+        const errorMessage = {
+          title: UPDATE_FAIL,
+          info: {
+            message: e.message,
+          },
+        };
+        res.send(errorMessage);
+      }
+    }
+});
+
+//! Patch Book
+BookController.route("patch/:id").patch(async (req, res) => {
+    try {
+    
+    } catch (e) {
+      if (e instanceof Error) {
+        const errorMessage = {
+          title: UPDATE_FAIL,
+          info: {
+            message: e.message,
+          },
+        };
+        res.send(errorMessage);
+      }
+    }
+});
+
+//! Delete Book
+BookController.route("/delete").delete(async (req, res) => {
+    try {
+    
+    } catch (e) {
+      if (e instanceof Error) {
+        const errorMessage = {
+          title: DELETE_FAIL,
+          info: {
+            message: e.message,
+          },
+        };
+        res.send(errorMessage);
+      }
+    }
+});
 module.exports = BookController;
