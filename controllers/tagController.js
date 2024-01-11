795dc1b6baf55966a70d9c612df533129c18e94a
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-11 09:32:29 -0500

    Building tag controller skeleton

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,4 +1,40 @@
 const Services = require('../services')
 const TagController = require("express").Router();
 
+// Constants for error messages or success messages are imported from a separate file
+const {
+    CREATE_SUCCESS,
+    CREATE_FAIL,
+    GET_SUCCESS,
+    GET_FAIL,
+    UPDATE_SUCCESS,
+    UPDATE_FAIL,
+    DELETE_FAIL,
+    DELETE_SUCCESS,
+    NOT_FOUND,
+    CONFLICT,
+  } = require("../controllers/constants");
+
+  //! Create Tag
+  TagController.route("").post(async (req, res) => {
+
+  });
+
+  //! Get All Tags
+  TagController.route("").get(async (req, res) => {
+
+  });
+  
+  //! Update Tag
+  TagController.route("").put(async (req, res) => {
+
+  });
+  
+  
+  //! Delete Tag
+  TagController.route("").delete(async (req, res) => {
+
+  });
+
+
 module.exports = TagController;
