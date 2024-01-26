f3a3b79bd002eb5d3698beea0c71db92760fdd60
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-26 15:44:37 -0500

    Added cascade delete

--- models/index.js
+++ models/index.js
@@ -9,19 +9,38 @@ const {
 } = require("../services/helpers/responseHandler");
 
 // Define associations between models
-UserModel.hasMany(ReviewModel);
-BookModel.hasMany(ReviewModel, { foreignKey: "bookId" }); // Ensure 'bookId' matches the foreign key in ReviewModel
+UserModel.hasMany(ReviewModel, {
+  onDelete: "CASCADE", // Cascade deletion to associated books
+  hooks: true, // Enable hooks to trigger before and after changes
+});
+BookModel.hasMany(
+  ReviewModel,
+  { foreignKey: "bookId" },
+  { onDelete: "CASCADE", hooks: true }
+);
 
 ReviewModel.belongsTo(UserModel, { foreignKey: "userId" });
 ReviewModel.belongsTo(BookModel, { foreignKey: "bookId" });
 
-BookModel.belongsTo(UserModel, { foreignKey: "userId" });
-ChapterModel.belongsTo(BookModel, { foreignKey: "bookId" });
+BookModel.belongsTo(
+  UserModel,
+  { foreignKey: "userId" },
+  { onDelete: "CASCADE", hooks: true }
+);
+ChapterModel.belongsTo(
+  BookModel,
+  { foreignKey: "bookId" },
+  { onDelete: "CASCADE", hooks: true }
+);
 
 BookModel.belongsToMany(TagModel, { through: "BookTags", as: "bookTags" });
 TagModel.belongsToMany(BookModel, { through: "BookTags", as: "tagBooks" });
 
-ChapterModel.belongsTo(UserModel, { foreignKey: "userId", as: "user" }); // New association
+ChapterModel.belongsTo(
+  UserModel,
+  { foreignKey: "userId", as: "user" },
+  { onDelete: "CASCADE", hooks: true }
+);
 
 module.exports = {
   UserModel,
